c9a50d83bfb24ac00e97a4f05fc916cb22bb6d72
Tanel Prikk <tanel.prikk@outlook.com>
2017-03-17 23:08:45 +0200

    postgresql added

--- scripts/schema_postgres.sql
+++ scripts/schema_postgres.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS lookup CASCADE;
 DROP TABLE IF EXISTS lookup_entity CASCADE;
 DROP TABLE IF EXISTS lookup_domain CASCADE;
 DROP TABLE IF EXISTS sentiment_type CASCADE;
-DROP TABLE IF EXISTS lookup_status CASCADE;
+DROP TABLE IF EXISTS lookup_state CASCADE;
 
 --------------------------------------------------------------------------------
 
@@ -15,10 +15,10 @@ CREATE TABLE lookup
 (
 	id BIGSERIAL NOT NULL,
 	lookup_entity_id BIGINT NOT NULL,
+	date TIMESTAMP(6) NOT NULL,
 
-	lookup_datetime TIMESTAMP(6) NOT NULL,
 	sentiment_type_code CHAR(3) NULL,
-	sentiment_lookup_status_code SMALLINT NOT NULL,
+	lookup_state_code SMALLINT NOT NULL,
 
 	CONSTRAINT pk_lookup PRIMARY KEY (id)
 );
@@ -28,12 +28,12 @@ CREATE TABLE sentiment_snapshot
 	id BIGSERIAL NOT NULL,
 	url VARCHAR(1000) NOT NULL,
 	title VARCHAR(255) NOT NULL,
-	snapshot_datetime TIMESTAMP(6) NOT NULL,
+	date TIMESTAMP(6) NOT NULL,
 	rank SMALLINT NOT NULL,
 	trust_level FLOAT(50) NOT NULL,
 
 	lookup_id BIGINT NOT NULL,
-	lookup_domain_id SMALLINT NOT NULL,
+	lookup_domain_code SMALLINT NOT NULL,
 	sentiment_type_code CHAR(3) NOT NULL,
 
 	CONSTRAINT pk_sentiment_snapshot PRIMARY KEY (id)
@@ -63,12 +63,12 @@ CREATE TABLE sentiment_type
 	CONSTRAINT pk_sentiment_type PRIMARY KEY (code)
 );
 
-CREATE TABLE lookup_status
+CREATE TABLE lookup_state
 (
 	code SMALLINT NOT NULL,
 	name VARCHAR(150) NOT NULL,
 
-	CONSTRAINT pk_lookup_status PRIMARY KEY (code)
+	CONSTRAINT pk_lookup_state PRIMARY KEY (code)
 );
 
 --------------------------------------------------------------------------------
@@ -81,15 +81,17 @@ FOREIGN KEY (sentiment_type_code)
 	REFERENCES sentiment_type (code);
 
 ALTER TABLE lookup
-ADD CONSTRAINT fk_lookup_lookup_status
-FOREIGN KEY (sentiment_lookup_status_code)
-	REFERENCES lookup_status (code);
+ADD CONSTRAINT fk_lookup_lookup_state
+FOREIGN KEY (lookup_state_code)
+	REFERENCES lookup_state (code);
 
 ALTER TABLE lookup
 ADD CONSTRAINT fk_lookup_lookup_entity
 	FOREIGN KEY (lookup_entity_id)
 		REFERENCES lookup_entity(id);
+		
 -------------
+
 ALTER TABLE sentiment_snapshot
 ADD CONSTRAINT fk_lookup_snapshot_lookup
 FOREIGN KEY (lookup_id)
@@ -97,14 +99,16 @@ FOREIGN KEY (lookup_id)
 
 ALTER TABLE sentiment_snapshot
 ADD CONSTRAINT fk_lookup_snapshot_lookup_domain
-FOREIGN KEY (lookup_domain_id)
+FOREIGN KEY (lookup_domain_code)
 	REFERENCES lookup_domain (code);
 
 ALTER TABLE sentiment_snapshot
 ADD CONSTRAINT fk_lookup_snapshot_sentiment_type
 FOREIGN KEY (sentiment_type_code)
 	REFERENCES sentiment_type (code);
+	
 -------------
+
 ------------------------------------------------------------------------------
  
 
@@ -113,3 +117,33 @@ FOREIGN KEY (sentiment_type_code)
 -- TODO: Margus
 
 ---------------------------------------------------------------------------	
+
+/* Classifier values */
+
+INSERT INTO lookup_state (code, name)
+VALUES (1, 'Queued');
+
+INSERT INTO lookup_state(code, name)
+VALUES (2, 'In progress');
+
+INSERT INTO lookup_state(code, name)
+VALUES (3, 'Complete');
+
+-------------
+
+INSERT INTO lookup_domain (code, name)
+VALUES (1, 'Google');
+
+-------------
+
+INSERT INTO sentiment_type (code, name)
+VALUES ('NEU', 'Neutral');
+
+INSERT INTO sentiment_type (code, name)
+VALUES ('POS', 'Positive');
+
+INSERT INTO sentiment_type (code, name)
+VALUES ('NEG', 'Negative');
+
+---------------------------------------------------------------------------
+
